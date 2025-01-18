093c7b8c4b5f82b875d63b2309820070e2670198
duc098-deptrai <duc651476@gmail.com>
2025-01-18 22:17:33 +0700

    edit

--- toolss.py
+++ toolss.py
@@ -1,34 +1,32 @@
 import requests
 import os , time,sys
 listfile = os.listdir()
-if  'api.py' in listfile and listfile in 'runfb.py':
-    pass
-else :
-    url = "https://raw.githubusercontent.com/duc098-deptrai/toolfb/refs/heads/main/apiencode.py"  # Đường dẫn đến file
-    output_file = "api.py"
-    response = requests.get(url, stream=True)  # Dùng stream để tải file lớn
-    with open(output_file, "wb") as file:
-        for chunk in response.iter_content(chunk_size=8192):  # Tải từng phần nhỏ
-            file.write(chunk)
-    url = "https://raw.githubusercontent.com/duc098-deptrai/toolfb/refs/heads/main/runfb_encode.py"  # Đường dẫn đến file
-    output_file = "runfb.py"
 
-    response = requests.get(url, stream=True)  # Dùng stream để tải file lớn
-    with open(output_file, "wb") as file:
-        for chunk in response.iter_content(chunk_size=8192):  # Tải từng phần nhỏ
-            file.write(chunk)
+url = "https://raw.githubusercontent.com/duc098-deptrai/toolfb/refs/heads/main/apiencode.py"  # Đường dẫn đến file
+output_file = "api.py"
+response = requests.get(url, stream=True)  # Dùng stream để tải file lớn
+with open(output_file, "wb") as file:
+    for chunk in response.iter_content(chunk_size=8192):  # Tải từng phần nhỏ
+        file.write(chunk)
+url = "https://raw.githubusercontent.com/duc098-deptrai/toolfb/refs/heads/main/runfb_encode.py"  # Đường dẫn đến file
+output_file = "runfb.py"
 
-    def loading_bar():
-        total = 100  # Tổng số phần trăm của thanh tiến trình
-        bar_length = 50  # Độ dài của thanh tiến trình
+response = requests.get(url, stream=True)  # Dùng stream để tải file lớn
+with open(output_file, "wb") as file:
+    for chunk in response.iter_content(chunk_size=8192):  # Tải từng phần nhỏ
+        file.write(chunk)
 
-        for i in range(total + 1):
-            percent = (i / total) * 100
-            bar = '█' * int((i / total) * bar_length)  # Tạo phần đã đầy
-            spaces = ' ' * (bar_length - len(bar))  # Tạo phần còn lại
-            sys.stdout.write(f'\rLoading... [{bar}{spaces}] {percent:.2f}%')  # In thanh tiến trình
-            sys.stdout.flush()
-            time.sleep(0.1)  # Đợi một chút để thanh tiến trình đầy từ từ
+def loading_bar():
+    total = 100  # Tổng số phần trăm của thanh tiến trình
+    bar_length = 50  # Độ dài của thanh tiến trình
+
+    for i in range(total + 1):
+        percent = (i / total) * 100
+        bar = '█' * int((i / total) * bar_length)  # Tạo phần đã đầy
+        spaces = ' ' * (bar_length - len(bar))  # Tạo phần còn lại
+        sys.stdout.write(f'\rLoading... [{bar}{spaces}] {percent:.2f}%')  # In thanh tiến trình
+        sys.stdout.flush()
+        time.sleep(0.1)  # Đợi một chút để thanh tiến trình đầy từ từ
         sys.stdout.write('\nDone!')
     print('đang tải tài nguyên')
     loading_bar()
