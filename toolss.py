fba30dea9feff38baa43147ff6df2de8a7bbd52c
duc098-deptrai <duc651476@gmail.com>
2025-04-17 17:40:06 +0700

    Update toolss.py
    finish

--- toolss.py
+++ toolss.py
@@ -1,2 +1,4 @@
 do = '\033[31m'
-print(f"{do}tool đã đóng . Vui lòng liên hệ admin")
+print(f"{do}tool đã đóng . Vui lòng liên hệ admin",end = '\n)
+print("FB : https://www.facebook.com/hhduccuti ")
+print("YT : https://www.youtube.com/@M%C3%A8ochill-s8o")
